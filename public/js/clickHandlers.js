6138494c0986487ab3b6f4ab54e981a8734373ca
vukoviciv <ivana.vukovic.hr@gmail.com>
2017-09-20 21:40:42 +0200

    fetch new when scroll to bottom. :bug: multiple firing!

--- public/js/clickHandlers.js
+++ public/js/clickHandlers.js
@@ -1,11 +1,15 @@
-function handleOnClickLoadMore(page) {
-	fetch(`/tweets/${page}`)
-		.then(res => res.json())
-		.then(data => data.tweets.map(tweet => getTweetHTMLcomponent(tweet)))
-		.then(tweets => {
-			let listElement = document.getElementById('tweet-items-container');
-			tweets.forEach(el => listElement.insertAdjacentHTML('beforeend', el) )
-		});
+window.page = 1;
+window.fetching = false;
+
+function fetchNewPage() {
+	console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+		fetch(`/tweets/${page}`)
+			.then(res => res.json())
+			.then(data => data.tweets.map(tweet => getTweetHTMLcomponent(tweet)))
+			.then(tweets => {
+				let listElement = document.getElementById('tweet-items-container');
+				tweets.forEach(el => listElement.insertAdjacentHTML('beforeend', el) )			
+			});
 }
 
 function getTweetHTMLcomponent(tweet) {
@@ -16,3 +20,19 @@ function getTweetHTMLcomponent(tweet) {
 
 	return liItem;
 }
+
+window.addEventListener('scroll', loadMoreAtTheBottom);
+
+
+
+
+function loadMoreAtTheBottom() {
+	console.log(window.innerHeight + window.scrollY);
+	console.log(document.body.offsetHeight);
+	if((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 50)	{
+			setTimeout(function() {
+				fetchNewPage();
+			}, 500);
+	}
+}
+
