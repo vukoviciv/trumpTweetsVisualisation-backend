ca04a36104838fb35cc56b5c2a1dd09f9abb91cf
Ivana Vukovic <ivana.st@windowslive.com>
2017-09-21 15:57:32 +0200

    first analyse of the data...

--- routes/graph.js
+++ routes/graph.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const tweetRepository = require('../repositories/tweetRepository');
+const analysedataAnalyser = require('../lib/analyseData');
+let { words } = require('../lib/options');
+
 
 router.get('/', (req, res) => {
-    tweetRepository.fetchAll()
-        .then(data => {
-
-            res.render('graph', {tweets: JSON.stringify(data)})
-        })
+    tweetRepository.fetchAll() // swith to fetchAll after
+        .then(data => analysedataAnalyser.extractTextProperties(data))
+        .then(textArray => analysedataAnalyser.analyseText(textArray, words))
+        .then(analysedText => res.render('graph', {tweets: JSON.stringify(analysedText)}))
         .catch((err) => res.render('error', {error: err}));
 });
 
-module .exports = router;
+module.exports = router;
