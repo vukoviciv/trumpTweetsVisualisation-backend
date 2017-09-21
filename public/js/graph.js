0c8066361f559bf23d47b2001173c041b21f1f73
Ivana Vukovic <ivana.st@windowslive.com>
2017-09-21 17:16:07 +0200

    prototype for graph. :bug: original array is mutable

--- public/js/graph.js
+++ public/js/graph.js
@@ -2,4 +2,24 @@ let svg = d3.select('svg')
     .attr('width', 960)
     .attr('height', 960);
 
+let circle = svg.append('g')
+    .selectAll('rect')
+    .data(tweets)
+    .enter()
+    .append('circle')
+    .attr('r', d => d.count)
+    .attr('transform', d => `translate(${d.count*10},${d.count})`)
+    .attr('fill', 'none')
+    .attr('stroke', 'red')
+    .text(d => d.word)
+    ;
+
+let text = svg.append('g')
+    .selectAll('text')
+    .data(tweets)
+    .enter()
+    .append('text')
+    .attr('transform', d => `translate(${d.count*10},${d.count})`)
+    .text(d => d.word)
+    ;
 
--- routes/graph.js
+++ routes/graph.js
@@ -2,13 +2,18 @@ const express = require('express');
 const router = express.Router();
 const tweetRepository = require('../repositories/tweetRepository');
 const analysedataAnalyser = require('../lib/analyseData');
-let { words } = require('../lib/options');
+const { words } = require('../lib/options');
 
 
 router.get('/', (req, res) => {
-    tweetRepository.fetchAll() // swith to fetchAll after
+    tweetRepository.fetchAll() // switch to fetchAll after
         .then(data => analysedataAnalyser.extractTextProperties(data))
-        .then(textArray => analysedataAnalyser.analyseText(textArray, words))
+        .then(textArray => {
+            //console.log("words: ", words);
+            //let w = [...words];
+            //console.log("w: ", w);
+            return analysedataAnalyser.analyseText(textArray, words)
+        })
         .then(analysedText => res.render('graph', {tweets: JSON.stringify(analysedText)}))
         .catch((err) => res.render('error', {error: err}));
 });
