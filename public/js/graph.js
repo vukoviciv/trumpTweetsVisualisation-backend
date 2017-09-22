9f8d22e05d46d033246c729879f6866aad535693
Ivana Vukovic <ivana.st@windowslive.com>
2017-09-22 15:18:56 +0200

    first graph rendering finished.

--- public/js/graph.js
+++ public/js/graph.js
@@ -1,25 +1,57 @@
+let width = window.innerWidth - 50,
+    height = window.innerHeight - 50;
+
+let nodes = tweets,
+    color = d3.scaleOrdinal(d3.schemeCategory10);
+
+const forceX = d3.forceX(width / 2).strength(0.045);
+const forceY = d3.forceY(height / 2).strength(0.045);
+
+let force = d3.forceSimulation()
+    .velocityDecay(0.2)
+    .force('x', forceX)
+    .force('y', forceY)
+    .force('collide', d3.forceCollide().radius(getRadius).iterations(1))
+    .nodes(nodes).on('tick', ticked);
+
 let svg = d3.select('svg')
-    .attr('width', 960)
-    .attr('height', 960);
+    .attr('width', width)
+    .attr('height', height);
 
-let circle = svg.append('g')
-    .selectAll('rect')
-    .data(tweets)
-    .enter()
-    .append('circle')
-    .attr('r', d => d.count)
-    .attr('transform', d => `translate(${d.count*10},${d.count})`)
-    .attr('fill', 'none')
-    .attr('stroke', 'red')
-    .text(d => d.word)
-    ;
+let circlesContainer = svg
+    .append('g')
+    .attr('class', 'circles-container');
 
-let text = svg.append('g')
+let textContainer = svg
+    .append('g')
+    .attr('class', 'text-container');
+
+let circles = circlesContainer
+    .selectAll('circle')
+    .data(nodes)
+    .enter().append('circle')
+    .attr('r', getRadius)
+    .style('fill', (d, i) => color(i))
+    .append('title')
+    .text(d => `${d.word} (${d.count})`);
+
+let texts = textContainer
     .selectAll('text')
-    .data(tweets)
-    .enter()
-    .append('text')
-    .attr('transform', d => `translate(${d.count*10},${d.count})`)
-    .text(d => d.word)
-    ;
+    .data(nodes.filter(node=>node.count > 30))
+    .enter().append('text')
+    .attr('text-anchor', 'middle')
+    .text(d => d.word);
 
+function getRadius(d) {
+    return d.count * 1.5;
+}
+
+function ticked(e) {
+    svg.selectAll('circle')
+        .attr('cx', d => d.x)
+        .attr('cy', d => d.y);
+
+    svg.selectAll('text')
+        .attr('x', d => d.x)
+        .attr('y', d => d.y);
+}
--- routes/graph.js
+++ routes/graph.js
@@ -8,9 +8,7 @@ const { words } = require('../lib/options');
 router.get('/', (req, res) => {
     tweetRepository.fetchAll() // switch to fetchAll after
         .then(data => analysedataAnalyser.extractTextProperties(data))
-        .then(textArray => {
-            return analysedataAnalyser.analyseText(textArray, words)
-        })
+        .then(textArray => analysedataAnalyser.analyseText(textArray, words))
         .then(analysedText => res.render('graph', {tweets: JSON.stringify(analysedText)}))
         .catch((err) => res.render('error', {error: err}));
 });
