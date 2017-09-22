9d78105807aa16155429fe4a2b0f3bcc29f4433e
Ivana Vukovic <ivana.st@windowslive.com>
2017-09-22 16:06:32 +0200

    small fixes

--- public/js/graph.js
+++ public/js/graph.js
@@ -1,5 +1,5 @@
-let width = window.innerWidth - 50,
-    height = window.innerHeight - 50;
+let width = window.innerWidth-10,
+    height = window.innerHeight-10;
 
 let nodes = tweets,
     color = d3.scaleOrdinal(d3.schemeCategory10);
@@ -42,16 +42,42 @@ let texts = textContainer
     .attr('text-anchor', 'middle')
     .text(d => d.word);
 
+/* LEGEND */
+
+let legendContainer = svg
+    .append('g')
+    .attr('class', 'legend-container')
+    .attr('x', 0)
+    .attr('y', 50);
+
+legendContainer.selectAll('rect')
+    .data(nodes)
+    .enter().append('rect')
+    .style('fill', (d, i) => color(i))
+    .attr('width', 20)
+    .attr('height', 20)
+    .attr('x', 50)
+    .attr('y', (d, i) => 30 * (i + 2));
+
+legendContainer.selectAll('text')
+    .data(nodes)
+    .enter().append('text')
+    .text(d => d.word)
+    .attr('x', 80)
+    .attr('y', (d, i)=> (30 * (i + 2) + 15));
+
+/* HELPER FUNCTIONS */
+
 function getRadius(d) {
-    return d.count * 1.5;
+    return d.count;
 }
 
 function ticked(e) {
-    svg.selectAll('circle')
+    circlesContainer.selectAll('circle')
         .attr('cx', d => d.x)
         .attr('cy', d => d.y);
 
-    svg.selectAll('text')
+    textContainer.selectAll('text')
         .attr('x', d => d.x)
         .attr('y', d => d.y);
 }
