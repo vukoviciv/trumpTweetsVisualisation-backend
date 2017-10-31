454908aa80ffdcaa910f495221c86de60a2fc0b0
Ivana Vukovic <ivana.st@windowslive.com>
2017-10-31 14:05:38 +0100

    add bulma. add modal

--- routes/graph.js
+++ routes/graph.js
@@ -1,15 +1,22 @@
 const express = require('express');
-const router = express.Router();
 const tweetRepository = require('../repositories/tweetRepository');
 const analysedataAnalyser = require('../lib/analyseData');
 const { words } = require('../lib/options');
 
+const router = express.Router();
+
 router.get('/', (req, res) => {
-    tweetRepository.fetchAll() // switch to fetchAll after
-        .then(data => analysedataAnalyser.extractTextProperties(data))
-        .then(textArray => analysedataAnalyser.analyseText(textArray, words))
-        .then(analysedText => res.render('graph', {tweets: JSON.stringify(analysedText)}))
-        .catch((err) => res.render('error', {error: err}));
+  tweetRepository.fetchAll()
+    .then(data => analysedataAnalyser.extractTextProperties(data))
+    .then(textArray => analysedataAnalyser.analyseText(textArray, words))
+    .then(analysedText => res.render('graph', {
+      tweets: JSON.stringify(analysedText),
+    }))
+    .catch(err => res.render('error', { error: err }));
+});
+
+router.get('/words', (req, res) => {
+  res.send('bravo');
 });
 
 module.exports = router;
