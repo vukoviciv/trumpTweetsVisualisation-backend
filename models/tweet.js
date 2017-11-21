91ae5c61abbcfe57f803f847d90b8ba7519c3684
Ivana Vukovic <ivana.st@windowslive.com>
2017-11-21 16:15:03 +0100

    eslint tweet and user models

--- models/tweet.js
+++ models/tweet.js
@@ -1,18 +1,9 @@
-'use strict';
-
-module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('Tweet', {
-        id_str: DataTypes.STRING,
-        full_text: DataTypes.TEXT,
-        source: DataTypes.STRING,
-        retweet_count: DataTypes.BIGINT,
-        favorite_count: DataTypes.BIGINT,
-        created_at: DataTypes.STRING
-    }, {
-        classMethods: {
-            associate: models => {
-                // define associations here
-            }
-        }
-    });
-};
+module.exports = (sequelize, DataTypes) =>
+  sequelize.define('Tweet', {
+    id_str: DataTypes.STRING,
+    full_text: DataTypes.TEXT,
+    source: DataTypes.STRING,
+    retweet_count: DataTypes.BIGINT,
+    favorite_count: DataTypes.BIGINT,
+    created_at: DataTypes.STRING,
+  });
